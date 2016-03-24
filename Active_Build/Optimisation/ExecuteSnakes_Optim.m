d6c9f43ebd2b7619c337ca9909db7d0b0d183ab8
Alexandre Payot <ap1949@bristol.ac.uk>
2016-03-24 17:43:16 +0000

    Development of Optimisation Process
    -> missing actual optimisation method to give new design variables at each step
    -> Need to workout the contour projection to limit curve

---
+++ Active_Build/Optimisation/ExecuteSnakes_Optim.m
@@ -0,0 +1,122 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%              University of Bristol
+%      Department of Aerospace Engineering
+%                     2015
+%
+%       Optimisation Using 
+%          Parametric Snakes for
+%      for Aerodynamic shape
+%         parametrisation
+%             Alexandre Payot
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+function [snaxel,snakposition,snakSave,looprestart,restartsnake,outinfo]...
+        =ExecuteSnakes_Optim(gridrefined,looprestart,baseGrid,connectstructinfo,param,outinfo,nIter,nProf)
+    % Executes the snakes edge detection process
+    %
+   
+    
+    procStr='SNAKE PROCESS';
+    [tStart]=PrintStart(procStr,2);
+    
+    varExtract={'refineSteps'};
+    [refineSteps]=ExtractVariables(varExtract,param);
+    
+    callerString='Snakes(gridrefined,looprestart,baseGrid,connectstructinfo,param);';
+    [textOut,snaxel,snakposition,snakSave,loopsnaxel,restartsnake]=evalc(callerString);
+    fprintf(textOut);
+    if length(loopsnaxel)==length(looprestart)
+        for ii=1:length(loopsnaxel)
+            looprestart(ii).snaxel=loopsnaxel(ii).snaxel;
+        end
+    else
+        looprestart=loopsnaxel;
+    end
+    
+    
+    looprestart=SubdivisionSurface_Snakes(looprestart,refineSteps,param);
+    
+    outinfo=OptimisationOutput('profile',param,outinfo,nIter,nProf,looprestart,restartsnake,snakSave);
+    [~]=PrintEnd(procStr,2,tStart);
+end
+
+
+%% Print to screen functions
+
+function [tStart]=PrintStart(procStr,lvl)
+    
+    procStart=[procStr,' start'];
+    tStart=now;
+    switch lvl
+        case 1
+            disp('  ')
+            disp('--------------------------------------------------------------------------------------------')
+            disp('--------------------------------------------------------------------------------------------')
+            disp(procStart)
+            disp(datestr(tStart,0))
+            disp('--------------------------------------------------------------------------------------------')
+            disp('  ')
+            
+        case 2
+            disp('  ')
+            disp('-----------------------')
+            disp(procStart)
+            
+        case 3
+            disp('----------')
+            disp(procStart)
+    end
+    
+end
+
+function [tElapsed]=PrintEnd(procStr,lvl,tStart)
+    
+    procStart=[procStr,' end'];
+    tEnd=now;
+    tElapsed=tEnd-tStart;
+    switch lvl
+        case 1
+            disp('  ')
+            disp('--------------------------------------------------------------------------------------------')
+            disp(['    Time Elapsed:',datestr(tElapsed,'HH:MM:SS:FFF')]);
+            disp(procStart)
+            disp('--------------------------------------------------------------------------------------------')
+            disp('--------------------------------------------------------------------------------------------')
+            disp('  ')
+            
+        case 2
+            
+            
+            disp(['Time Elapsed:',datestr(tElapsed,'HH:MM:SS:FFF')]);
+            disp(procStart)
+            disp('-----------------------')
+            disp('  ')
+        case 3
+            disp('----------')
+            
+    end
+    
+end
+
+%% Subdivision process
+
+function [loop]=SubdivisionSurface_Snakes(loop,refineSteps,param)
+    % Function taking in a closed loop of vertices and applying the subdivision
+    % process
+    % typBOund is te type of boundary that is expected, it can either be the
+    % string 'vertex' (default) or the string 'snaxel' to show that the snaxel
+    % process has been used
+     varExtract={'typeBound','subdivType'};
+    [typeBound,subdivType]=ExtractVariables(varExtract,param);
+    if ~exist('typeBound','var'), typeBound='vertex'; end
+    
+    for ii=1:length(loop)
+        startPoints=loop(ii).(typeBound).coord;
+        loop(ii).isccw=CCWLoop(startPoints);
+        newPoints=SubDivision(startPoints,refineSteps,subdivType);
+        %newPoints=SubSurfBSpline(startPoints(1:end-2,:),refineSteps);
+        loop(ii).subdivision=newPoints;
+    end
+end
