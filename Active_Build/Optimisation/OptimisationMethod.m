9da00348b9213420dbf610755ae2e7f6a7b8a729
Alexandre Payot <ap1949@bristol.ac.uk>
2016-03-31 10:07:44 +0100

    Optimisation method in devellopment
    Key changes to come:
    -  writing the optimisation methods
    - Getting dp/dA

---
+++ Active_Build/Optimisation/OptimisationMethod.m
@@ -0,0 +1,14 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%              University of Bristol
+%      Department of Aerospace Engineering
+%                     2016
+%
+%            Optimisation Using 
+%          Parametric Snakes for
+%         for Aerodynamic shape
+%         parametrisation
+%         
+%             Alexandre Payot
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
