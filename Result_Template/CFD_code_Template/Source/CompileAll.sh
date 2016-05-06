0e8090c6f3fb42c7e2c9b89ddf9dc33845b1b47d
Alexandre Payot <ap1949@bristol.ac.uk>
2016-05-06 16:54:51 +0100

    Updated Fluid postprocessing and layout files

--- Result_Template/CFD_code_Template/Source/CompileAll.sh
+++ Result_Template/CFD_code_Template/Source/CompileAll.sh
@@ -4,6 +4,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $DIR
 
-gfortran -O3 -o cartcell.exe CartCellv27.f90
+gfortran -O3 -o cartcell.exe CartCellv28.f90
 gfortran -O3 -freal-8-real-16 -o eulerflowuns.exe EulerFlowUns14.f90
-gfortran -O3 -o postproc.exe pproc8.f90
+gfortran -O3 -o postproc.exe pproc9_AP.f90
