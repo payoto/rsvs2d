4a7b6ddde8f7a8c7c84de4bb576945999a89bef4
Alexandre Payot <ap1949@bristol.ac.uk>
2016-05-11 18:03:14 +0100

    AUTO-COMMIT For Backup Update

--- Active_Build/Optimisation/OptimisationMethod.m
+++ Active_Build/Optimisation/OptimisationMethod.m
@@ -12,10 +12,10 @@
 %             Alexandre Payot
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function [newPop,iterCurr,paramoptim]=OptimisationMethod(paramoptim,varargin)
+function [newPop,iterCurr,paramoptim,deltas]=OptimisationMethod(paramoptim,varargin)
     % Function distributing the optimisation to various methods
     
-    
+    deltas={};
     varExtract={'optimMethod'};
     [optimMethod]=ExtractVariables(varExtract,paramoptim);
     
@@ -28,9 +28,9 @@ function [newPop,iterCurr,paramoptim]=OptimisationMethod(paramoptim,varargin)
             [newPop,iterCurr,paramoptim]=DifferentialEvolution(paramoptim,proj,varargin{1},varargin{2});
             
         case 'conjgrad'
-            [newPop,iterCurr,paramoptim]=ConjugateGradient(paramoptim,varargin{1},varargin{2});
+            [newPop,iterCurr,paramoptim,deltas]=ConjugateGradient2(paramoptim,varargin{1},varargin{2});
         case 'conjgradls'
-            [newPop,iterCurr,paramoptim]=ConjugateGradientLS(paramoptim,varargin{1},varargin{2});
+            [newPop,iterCurr,paramoptim,deltas]=ConjugateGradient2(paramoptim,varargin{1},varargin{2});
         case 'DEStrip'
             
         case 'GA'
@@ -249,27 +249,6 @@ function [newPop,iterCurr,paramoptim]=ConjugateGradientLS(paramoptim,iterCurr,it
     paramoptim.optim.CG.lineSearch=~lineSearch;
 end
 
-function [newRootFill,desVarList]=OverflowHandling(paramoptim,desVarList,newRootFill)
-    % Function which handles steps overflowing the fill constraint
-    
-    varExtract={'desVarRange','varOverflow','notDesInd'};
-    [desVarRange,varOverflow,notDesInd]=ExtractVariables(varExtract,paramoptim);
-    
-    switch varOverflow
-        case 'truncate'
-            minD=min(desVarRange);
-            maxD=max(desVarRange);
-            newRootFill(newRootFill<minD)=minD;
-            newRootFill(newRootFill>maxD)=maxD;
-            
-        otherwise
-            error('No valid design variable overflow mechanism')
-            
-    end
-    
-    
-    
-end
 
 function [gradF]=ExtractFiniteGradient(popstruct,desVarList)
     
@@ -344,5 +323,339 @@ function [DxT]=FindStepLength(popstruct,direction,nPop)
     next=ceil(iNew);
     vec=popstruct(next).fill-popstruct(prec).fill;
     DxT=vec*(iNew-prec)+popstruct(prec).fill;
+    
+end
+function [newRootFill,desVarList]=OverflowHandling(paramoptim,desVarList,newRootFill)
+    % Function which handles steps overflowing the fill constraint
+    
+    varExtract={'desVarRange','varOverflow'};
+    [desVarRange,varOverflow]=ExtractVariables(varExtract,paramoptim);
+    
+    switch varOverflow
+        case 'truncate'
+            minD=min(desVarRange);
+            maxD=max(desVarRange);
+            newRootFill(newRootFill<minD)=minD;
+            newRootFill(newRootFill>maxD)=maxD;
+            
+        otherwise
+            error('No valid design variable overflow mechanism')
+            
+    end
+    
+    
+    
+end
+
+%%
+
+function [newPop,iterCurr,paramoptim,deltas]=ConjugateGradient2(paramoptim,iterCurr,iterm1)
+    
+     varExtract={'diffStepSize','direction','notDesInd','desVarRange',...
+        'lineSearch','worker','nPop','validVol','varActive'};
+    
+    [diffStepSize,direction,notDesInd,desVarRange,lineSearch,worker,...
+        nPop,validVol,varActive]=ExtractVariables(varExtract,paramoptim);
+    
+    
+    % Extract previous iteration information
+    [gradstruct_curr]=GetIterationInformation(iterCurr);
+    [gradstruct_m1]=GetIterationInformation(iterm1);
+    
+    
+    % Case dependant statements
+    if lineSearch
+        [stepVector]=FindOptimalStepVector(iterstruct,worker,direction);
+       [newRoot,deltaRoot]=GenerateNewRootFill(rootFill,stepVector);
+       % Need to build function for activation and deactivation of variables
+       [inactiveVar]=SelectInactiveVariables(newRoot,varActive);
+       [desVarList]=ExtractActiveVariable(length(iterCurr(1).fill),notDesInd,inactiveVar);
+       [newGradPop,deltaGrad]=GenerateNewGradientPop(newRoot,desVarRange,diffStepSize,desVarList);
+       newPop=[newRoot;newGradPop];
+       deltas=[deltaRoot,deltaGrad];
+       
+       paramoptim.optim.CG.lineSearch=false;
+    else % Direction Search
+        
+        % Get component change
+        % The assumption is the the modes have been selected sensibly and
+        % will be close to orthogonal
+        [modestruct]=ExtractModes(gradstruct_curr,gradstruct_m1);
+        [gradF_curr,gradF_m1]...
+            =BuildGradientVectors(gradstruct_curr,gradstruct_m1,modestruct);
+        
+        % Get Corresponding design vector direction
+        [stepVector]=NewStepDirection...
+            (gradF_curr,gradF_m1,modestruct,iterCurr,direction);
+        % Generate Linesearch Distances
+        rootPop=iterCurr(1).fill;
+        [stepLengths]=StepLengthsForLS(rootPop,stepVector,worker,validVol,desVarRange);
+        [newPop,deltas]=...
+        GenerateNewLineSearchPop(rootPop,stepVector,stepLengths,paramoptim);
+        % Population trimming for invalid values
+        
+        % Declare linesearch
+        paramoptim.optim.CG.lineSearch=true;
+    end
+    
+    % Securing the end
+    [nPop,~]=size(newPop);
+    paramoptim.general.nPop=nPop;
+    
+end
+
+function [gradientopt]=GetIterationInformation(population)
+    
+    nPop=length(population);
+    nGrad=nPop-1;
+    nFill=length(population(1).fill);
+    
+    rootPop=population(1).fill;
+    rootObj=population(1).objective;
+    
+    gradientopt=struct('design',zeros(1,nFill),'objective',0,'fill',zeros(1,nFill));
+    gradientopt=repmat(gradientopt,[1,nGrad]);
+    
+    for ii=1:nGrad
+        
+        gradientopt(ii).design(population(ii+1).optimdat.var)...
+            =population(ii+1).optimdat.value;
+        gradientopt(ii).objective=population(ii+1).objective-rootObj;
+        gradientopt(ii).fill=population(ii+1).fill-rootPop;
+        
+    end
+    
+    
+    
+end
+
+function [modestruct]=ExtractModes(gradstruct_curr,gradstruct_m1)
+    
+    nCurr=length(gradstruct_curr);
+    desModes_curr=vertcat(gradstruct_curr(:).design);
+    nM1=length(gradstruct_m1);
+    desModes_m1=vertcat(gradstruct_m1(:).design);
+    
+    allModes=[desModes_curr;desModes_m1];
+    
+    modeSimilarity=FindIdenticalVectorOrd(allModes);
+    
+    nModes=length(modeSimilarity);
+    
+    modestruct=struct('mode',zeros(size(gradstruct_curr(1).design)),...
+        'curr',[],'m1',[]);
+    
+    modestruct=repmat(modestruct,[1,nModes]);
+    
+    for ii=1:nModes
+        for jj=1:length(modeSimilarity{ii})
+            if modeSimilarity{ii}(jj)<=nCurr
+                modestruct(ii).curr=[modestruct(ii).curr,modeSimilarity{ii}(jj)];
+            elseif modeSimilarity{ii}(jj)<=(nCurr+nM1)
+                modestruct(ii).m1=[modestruct(ii).m1,modeSimilarity{ii}(jj)-nCurr];
+            else
+                error('Invalid Index was produced by the mode identification')
+            end
+        end
+    end
+    
+    
+end
+
+function [gradF_curr,gradF_m1]=...
+        BuildGradientVectors(gradstruct_curr,gradstruct_m1,modestruct)
+    
+    
+    nModes=length(modestruct);
+    
+    gradF_curr=zeros([nModes,1]);
+    gradF_m1=zeros([nModes,1]);
+    
+    for ii=1:nModes
+        
+        currInd=modestruct(ii).curr;
+        m1Ind=modestruct(ii).curr;
+        if ~isempty(currInd)
+            gradF_curr(ii)=gradstruct_curr(currInd(1)).objective;
+        end
+        if ~isempty(m1Ind)
+            gradF_m1(ii)=gradstruct_m1(m1Ind(1)).objective;
+        end
+    end
+    
+    
+    
+end
+
+function [stepVector]=NewStepDirection(gradF_curr,gradF_m1,modestruct,iterCurr,direction)
+    
+    normVec=@(v) sqrt(sum(v.^2,2));
+    
+    prevStep=zeros(size(iterCurr(1).fill));
+    prevStep(iterCurr(1).optimdat.var)=iterCurr(1).optimdat.value;
+    
+    modes=vertcat(modestruct(:).mode)';
+    
+    gradDes_curr=(modes*gradF_curr)';
+    gradDes_m1=(modes*gradF_m1)';
+    
+    switch direction
+        case 'min'
+            signD=-1;
+        case 'max'
+            signD=1;
+    end
+    
+    stepVector=signD*gradDes_curr...
+        +(normVec(gradDes_curr)/normVec(prevStep))^2*prevStep;
+    
+    
+end
+
+function [stepLengths]=StepLengthsForLS(rootPop,stepVector,worker,validVol,desVarRange)
+    
+    
+    
+    % Limit Imposed by design variable range
+    minChange=min(desVarRange)-rootPop;
+    maxChange=max(desVarRange)-rootPop;
+    minMin=FindMinPosMov(minChange,stepVector);
+    minMax=FindMinPosMov(maxChange,stepVector);
+    % Limit imposed by the maximum step size
+    stepToLim=validVol*ones(size(stepVector))./abs(stepVector);
+    stepToLim=min(stepToLim(isfinite(stepToLim)));
+    
+    %
+    stepLow=0;
+    stepHigh=stepToLim;
+    
+    % Step Length will be bisecting backtrack
+    stepLengths=zeros([1,worker]);
+    stepLengths(end)=stepHigh;
+    
+    for ii=worker-1:-1:2
+        stepLengths(ii)=stepLengths(ii+1)/2;
+    end
+    
+end
+
+function [minChange]=FindMinPosMov(change,step)
+    
+    change=change./step;
+    change=change(isfinite(change));
+    change=change(change>0);
+    minChange=min(change);
+end
+
+function [newPop,deltas]=...
+        GenerateNewLineSearchPop(rootPop,stepVector,stepLengths,paramoptim)
+
+    nNew=length(stepLengths);
+    nFill=length(rootPop);
+    deltaDes=(stepLengths'*stepVector);
+    newPop=(ones([nNew,1])*rootPop)+deltaDes;
+    
+    for ii=nNew:-1:1
+        actVar=find(deltaDes(ii,:));
+        deltas{ii}=[actVar;deltaDes(ii,actVar)];
+        [newPop(ii,:)]=OverflowHandling2(paramoptim,newPop(ii,:));
+    end
+    
+end
+
+function [newRootFill]=OverflowHandling2(paramoptim,newRootFill)
+    % Function which handles steps overflowing the fill constraint
+    
+    varExtract={'desVarRange','varOverflow'};
+    [desVarRange,varOverflow]=ExtractVariables(varExtract,paramoptim);
+    
+    switch varOverflow
+        case 'truncate'
+            minD=min(desVarRange);
+            maxD=max(desVarRange);
+            newRootFill(newRootFill<minD)=minD;
+            newRootFill(newRootFill>maxD)=maxD;
+            
+        otherwise
+            error('No valid design variable overflow mechanism')
+            
+    end
+    
+    
+    
+end
+
+function [stepVector]=FindOptimalStepVector(iterstruct,worker,direction)
+    
+    f=[iterstruct(:).objective];
+    stepLengths=1./[inf,(worker-1):-1:1];
+    vec=zeros(size(iterstruct(1).fill));
+    vec(iterstruct(end).optimdat.var)=iterstruct(end).optimdat.value;
+    
+    pp=spline(stepLengths,f);
+    iTest=linspace(0,1,1000);
+    switch direction
+        case 'min'
+            [targObj,indexLoc]=min(ppval(pp,iTest));
+        case 'max'
+            [targObj,indexLoc]=max(ppval(pp,iTest));
+    end
+    stepLength=iTest(indexLoc);
+    
+    if stepLength==0
+        warning('Step Length is stagnant this iteration')
+    end
+    
+    stepVector=vec*stepLength;
+    
+end
+
+function [newRoot,deltas]=GenerateNewRootFill(rootFill,stepVector)
+    
+    newRoot=rootFill+stepVector;
+    [newRoot]=OverflowHandling2(paramoptim,newRoot);
+    
+    realStep=newRoot-rootFill;
+    
+    actVar=find(realStep~=0);
+    deltas{1}=[actVar;realStep(actVar)];
+    
+end
+
+function [inactiveVar]=SelectInactiveVariables(newFill,varActive)
+    
+    switch varActive
+        case 'all'
+            inactiveVar=[];
+        case 'border'
+            error('Not coded yet')
+            
+        case 'wideborder'
+            error('Not coded yet')
+            
+        otherwise
+            error('unrecognised variable activation criterion')
+    end
+    
+    
+end
+
+function [newGradPop,deltas]=GenerateNewGradientPop(rootFill,desVarRange,stepSize,desVarList)
+    
+    nActVar=length(desVarList);
+    
+    newGradPop=(ones(nActVar,1)*rootFill);
+    
+    negMov=find(rootFill(desVarList)>=(max(desVarRange)-stepSize));
+    signMat=eye(nActVar);
+    signMat(:,negMov)=signMat(:,negMov)*-1;
+    
+    partialSteps=signMat*stepSize;
+    newGradPop(:,desVarList)=newGradPop(:,desVarList)+partialSteps;
+    
+    for ii=nActVar:-1:1
+        actVar=find(partialSteps(ii,:)~=0);
+        deltas{ii}=[desVarList(actVar);partialSteps(ii,actVar)];
+    end
     
 end
