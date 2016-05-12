86c17bd4c01528be359a38e30d4cee6376bff6c2
Alexandre Payot <ap1949@bristol.ac.uk>
2016-05-12 18:08:10 +0100

    AUTO-COMMIT For Backup Update

---
+++ param_2016_05_11T195857_Test_CG_Aero_desktop.m
@@ -0,0 +1,43 @@
+% Parameter File 
+% 11-May-2016 19:58:57 
+% 2016-05-11T195857_Test_CG_Aero_desktop 
+
+param.general.optimCase = ['Test_CG_Aero_desktop']; 
+param.general.paramCase = ['optimSupersonic']; 
+param.general.optimMethod = ['conjgradls']; 
+param.general.desVarRange = [0,1]; 
+param.general.nPop = [6]; 
+param.general.startPop = ['randuniform']; 
+param.general.maxIter = [6]; 
+param.general.worker = [8]; 
+param.general.objectiveName = ['CutCellFlow']; 
+param.general.direction = ['min']; 
+param.general.defaultVal = [1000]; 
+param.general.knownOptim = [0.14609]; 
+param.general.restartSource = ''; 
+param.general.symType = ['horz']; 
+param.general.nDesVar = [0]; 
+param.general.symDesVarList = [ ]; 
+param.general.notDesInd = [ ]; 
+param.general.iterGap = [1]; 
+param.optim.DE.diffAmplification = [0.5]; 
+param.optim.DE.xOverRatio = [0.5]; 
+param.optim.DE.geneType = ['single']; 
+param.optim.CG.diffStepSize = [0.01]; 
+param.optim.CG.varOverflow = ['truncate']; 
+param.optim.CG.varActive = ['all']; 
+param.optim.CG.lineSearch = [false]; 
+param.optim.CG.validVol = [0.3]; 
+param.spline.splineCase = ['aerosnake']; 
+param.spline.domain = ['normalizeX']; 
+param.obj.flow.CFDfolder = ['C:\Users\ap1949\Local Documents\PhD\Development Work\Snakes Volume Parametrisation\source\Result_Template\CFD_code_Template\supersonic_ogive']; 
+param.obj.flow.stoponerror = [false]; 
+param.obj.flow.targConv = [-6]; 
+param.obj.flow.lengthConvTest = [100]; 
+param.obj.flow.restartIter = [1000]; 
+param.obj.flow.maxRestart = [5]; 
+param.obj.flow.nMach = [2]; 
+param.constraint.desVarConstr = {['MeanVolFrac']}; 
+param.constraint.desVarVal = {[0.4]}; 
+param.constraint.resConstr = {['AeroResidual']}; 
+param.constraint.resVal = {[-3.5]}; 
