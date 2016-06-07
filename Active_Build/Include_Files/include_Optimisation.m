fadfbf552888a1c8f48e3efed4a9175f9ed28753
Alexandre Payot <ap1949@bristol.ac.uk>
2016-06-07 18:00:00 +0100

    AUTO-COMMIT For Backup Update

--- Active_Build/Include_Files/include_Optimisation.m
+++ Active_Build/Include_Files/include_Optimisation.m
@@ -20,20 +20,17 @@ end
 
 
 
-
 function [inactiveVar]=SelectInactiveVariables(newFill,varActive)
     
     switch varActive
         case 'all'
             inactiveVar=[];
         case 'border'
-            is0=newFill==0;
-            is1=newFill==1;
-            
-            inactiveVar=find(is0);
-            inactiveVar=[inactiveVar,find(is1)];
+            [inactiveVar]=InactiveVariables_border(newFill);
             
         case 'wideborder'
+            
+            [inactiveVar,activeVar]=InactiveVariables_border(newFill);
             error('Not coded yet')
             
         otherwise
@@ -42,3 +39,31 @@ function [inactiveVar]=SelectInactiveVariables(newFill,varActive)
     
     
 end
+
+function [inactiveVar,activeVar]=InactiveVariables_border(newFill)
+
+    is0=newFill==0;
+    is1=newFill==1;
+    
+    inactiveVar=find(is0);
+    inactiveVar=[inactiveVar,find(is1)];  
+    
+    isBord=~is0 & ~is1;
+    activeVar=find(isBord);
+    
+    
+end
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
