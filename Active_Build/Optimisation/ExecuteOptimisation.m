5221a5fa9abefd641ea50e1226e395cd10bde44e
Alexandre Payot <ap1949@bristol.ac.uk>
2016-09-07 18:00:03 +0100

    AUTO-COMMIT For Backup Update

--- Active_Build/Optimisation/ExecuteOptimisation.m
+++ Active_Build/Optimisation/ExecuteOptimisation.m
@@ -1132,13 +1132,9 @@ end
 
 function [objValue,additional]=LengthArea(paramoptim,member,loop)
     for ii=1:length(loop)
-        points=loop(ii).snaxel.coord(1:end-1,:);
-        [A(ii)]=abs(CalculatePolyArea(points));
-        vec=points([end,1:end-1],:)-points;
-        L(ii)=sum(sqrt(sum(vec.^2,2)));
-        t(ii)=max(points(:,2))-min(points(:,2));
-        xMin(ii)=min(points(:,1));
-        xMax(ii)=max(points(:,1));
+        
+        [xMin(ii),xMax(ii),t(ii),L(ii),A(ii)]=...
+            ClosedLoopProperties(loop(ii).snaxel.coord(1:end-1,:));
         
     end
     objValue=sum(A)/sum(L);
@@ -1166,23 +1162,3 @@ function [objValue,additional]=CutCellFlow(paramoptim,member,loop)
     
 end
 
-function [A]=CalculatePolyArea(points)
-    
-    pointsVec=points';
-    pointsVec=pointsVec(:);
-    plot(points(:,1),points(:,2));
-    n=length(points(:,1));
-    centreMat=eye(2*n);
-    centreMat=(centreMat+centreMat(:,[end-1:end,1:end-2]))*0.5;
-    
-    [rotDif]=[0 -1 0 1; 1 0 -1 0];
-    normMat=zeros(2*n);
-    for ii=1:n-1
-        normMat((2*(ii-1)+1):(2*(ii-1)+2),(2*(ii-1)+1):(2*(ii-1)+4))=rotDif;
-    end
-    ii=n;
-    normMat((2*(ii-1)+1):(2*(ii-1)+2),(2*(ii-1)+1):(2*(ii-1)+2))=rotDif(:,1:2);
-    normMat((2*(ii-1)+1):(2*(ii-1)+2),1:2)=rotDif(:,3:4);
-    A=0.5*(normMat*pointsVec)'*(centreMat*pointsVec);
-    
-end
