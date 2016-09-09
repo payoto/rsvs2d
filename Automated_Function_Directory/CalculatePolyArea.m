b48667316a9556cc45ce3677eb17727bc240e43e
Alexandre Payot <ap1949@bristol.ac.uk>
2016-09-10 00:46:38 +0100

    AUTO-COMMIT For Backup Update

--- Automated_Function_Directory/CalculatePolyArea.m
+++
@@ -1,7 +0,0 @@
-function [varargout]=CalculatePolyArea(varargin)
-global CalculatePolyArea_Handle
-nOut=nargout(CalculatePolyArea_Handle);
-nOutReq=nargout;
-nOut(nOut<0)=nOutReq;
-[varargout{1:nOut}]=CalculatePolyArea_Handle(varargin{:});
-end
