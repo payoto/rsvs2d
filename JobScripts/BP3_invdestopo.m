eefcd7a26b0d91cb9490ae2d21517eb064c6b3b6
Alexandre Payot <ap1949@bristol.ac.uk>
2017-10-30 11:53:54 +0000

    HPC deployment New topo inverse design cases

--- JobScripts/BP3_invdestopo.m
+++ JobScripts/BP3_invdestopo.m
@@ -1,7 +1,7 @@
 function BP3_invdestopo(refCrit,cornAct,aeroStr,numRat)
     MoveToDir('source',1)
     InitialiseSnakeFlow;
-    numRat=floor((numRat-1)/5);
+    numRat=floor((numRat-1)/3);
     disp(sprintf('invdestopo(''%s'',%i,''%s'',%i)',refCrit,cornAct,aeroStr,numRat))
     ExecuteOptimisation(sprintf('invdestopo(''%s'',%i,''%s'',%i)',refCrit,cornAct,aeroStr,numRat));
     
