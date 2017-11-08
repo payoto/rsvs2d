0c0ed213b486fb9033ec4b58031e88cd01a595db
Alexandre Payot <ap1949@bristol.ac.uk>
2017-11-08 18:00:02 +0000

    AUTO-COMMIT For Backup Update

--- Active_Build/Postprocessing/QuickFigSave.m
+++ Active_Build/Postprocessing/QuickFigSave.m
@@ -1,5 +1,6 @@
 function []=QuickFigSave(figList)
     
+    addpath ..\..\matlabpackages\export_fig
     for ii=figList;
         h=findobj(ii);
         h(1).Renderer='opengl';
@@ -10,10 +11,16 @@ function []=QuickFigSave(figList)
             axh(jj).Color='none';
         end
         figName=matlab.lang.makeValidName([h(1).Name,int2str(ii)]);
-        print(h(1),'-r300','-dpng',['.\fig\',figName,'.png'])
+        %print(h(1),'-r300','-dpng',['.\fig\',figName,'.png'])
+        export_fig(h(1),'-transparent','-r300',['.\fig\',figName,'.png'])
         h(1).Renderer='painters';
         print(h(1),'-depsc',['.\fig\',figName,'.eps'])
-        print(h(1),'-dpdf',['.\fig\',figName,'.pdf'])
+        cell2Save={'PaperType','PaperUnits','PaperSize','PaperPosition'};
+        
+        
+        %print(h(1),'-dpdf',['.\fig\',figName,'.pdf'])
+        export_fig(h(1),'-transparent',['.\fig\',figName,'.pdf'])
+        
         h(1).Color=[0.93 0.93 0.93];
         for jj=1:numel(axh)
             axh(jj).Color=hCol{jj};
