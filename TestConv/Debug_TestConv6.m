6150e26a6669e796273728094d17cb77918ac711
Alexandre Payot <ap1949@bristol.ac.uk>
2018-04-27 18:00:01 +0100

    added conv test plot function

---
+++ TestConv/Debug_TestConv6.m
@@ -0,0 +1,22 @@
+
+InitialiseSnakeFlow
+ExecInclude
+
+addpath('/panfs/panasas01/aero/ap1949/ASO_LK/SRC')
+addpath('/panfs/panasas01/aero/ap1949/ASO_LK/SRC/matlab-snopt')
+ 
+ profVecNoAdj=[62,72,65,66,77,51,8]; % all sensible
+ profVecNoDir=[37,20,33,40,50]; % 33-50 are weird
+ profVecWorks=[4 10 13 16 ]; % all sensible
+ pathStr='/panfs/panasas01/aero/ap1949/SnakVolParam/results/Optimisation/Archive_2018_04/Day_2018-04-23/Dir_2018-04-23T102550_ASOMS_subdiv_5_basis_1_';
+cflNum=[1 2 4 7];
+cflNumAdj=[0.5 1 1.5 2]*0.9;
+for ii=1:numel(cflNum)
+    for jj=1:numel(cflNumAdj)
+        
+        [ASOConvstruct{ii,jj},population{ii,jj}]=RunASOFlowConvTest(pathStr,...
+            '/panfs/panasas01/aero/ap1949/SnakVolParam/results/ConvSU2',2,...
+            [profVecNoAdj,profVecNoDir,profVecWorks],...
+            ['CFLADJimplicit(',num2str(cflNum(ii)),',',num2str(cflNumAdj(jj)),')']);
+    end
+end
---
+++ TestConv/Debug_TestConv7.m
@@ -0,0 +1,18 @@
+
+InitialiseSnakeFlow
+ExecInclude
+
+addpath('/panfs/panasas01/aero/ap1949/ASO_LK/SRC')
+addpath('/panfs/panasas01/aero/ap1949/ASO_LK/SRC/matlab-snopt')
+ 
+ profVecNoAdj=[62,72,65,66,77,51,8]; % all sensible
+ profVecNoDir=[37,20,33,40,50]; % 33-50 are weird
+ profVecWorks=[4 10 13 16 ]; % all sensible
+ pathStr='/panfs/panasas01/aero/ap1949/SnakVolParam/results/Optimisation/Archive_2018_04/Day_2018-04-23/Dir_2018-04-23T102550_ASOMS_subdiv_5_basis_1_';
+
+
+    [ASOConvstruct,population]=RunASOFlowConvTest(pathStr,...
+        '/panfs/panasas01/aero/ap1949/SnakVolParam/results/ConvSU2_4',2,...
+        1:100,['NewDefault']);
+
+save Debug_TestConv_NewDefault2
