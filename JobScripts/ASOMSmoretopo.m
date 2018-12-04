2b53e860bf8e80ebba7970c411a88059411f6d93
Alexandre Payot <alexandre.payotli@gmail.com>
2018-12-04 18:08:55 +0000

    Modify ASOMSmoretopo case to take 'vol' variable

--- JobScripts/ASOMSmoretopo.m
+++ JobScripts/ASOMSmoretopo.m
@@ -1,14 +1,13 @@
-function ASOMSmoretopo()
-    
+function ASOMSmoretopo(vol, step)
+    % vol = 0.12 or 0.2
     MoveToDir('source',1)
     InitialiseSnakeFlow;
     
-    callStr='ASOMS_moretopo(0.2)';
+    callStr=sprintf('ASOMS_moretopo(%.3f,%i)',vol,step);
     disp(callStr);
     
-   
     [~,pathHome]=system('echo -n $HOME');
-    restartPath=[pathHome,'/SnakVolParam/restarts/Restart_ASOMS_vol_0.200_moretopo.mat'];
+    restartPath=[pathHome,'/SnakVolParam/restarts/',sprintf('Restart_ASOMS_vol_%.3f',vol),'_moretopo.mat'];
     
     ExecuteOptimisation(callStr,{restartPath,{'none',true}});
     
