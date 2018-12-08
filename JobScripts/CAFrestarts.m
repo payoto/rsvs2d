df57990d0320a6ef248140112051f472a58e0442
Alexandre Payot <ap1949@bristol.ac.uk>
2018-12-08 01:35:07 +0000

    Prepare CAF reruns for BP3

---
+++ JobScripts/CAFrestarts.m
@@ -0,0 +1,21 @@
+function CAFrestarts(ii)
+    MoveToDir('source',1)
+    InitialiseSnakeFlow;
+    
+    restartFolder = '/panfs/panasas01/aero/ap1949/SnakVolParam/restarts/CAF/';
+    switch ii
+        case 1
+            caseStr = 'volsweeplocal(0.15,''uo'')';
+            pathMat = [restartFolder,'restart_015_caf_s5.mat'];
+            ExecuteOptimisation(caseStr,{pathMat, {'conjgrad',1}})
+            
+        case 2
+            caseStr = 'volsweeplocal(0.16,''uo'')';
+            pathMat = [restartFolder,'restart_016_caf_s3.mat'];
+            ExecuteOptimisation(caseStr,{pathMat, {'conjgrad',1}})
+    end
+    
+    
+    
+    
+end
