41341a0c70466df438ff1cc81b6b1090b46d6e89
alexandre payot <alexandre_payot@hotmail.com>
2019-10-02 20:36:28 +0200

    Fix naming in jobScript

--- JobScripts/BP3_arealiftbusesweep.m
+++ JobScripts/BP3_arealiftbusesweep.m
@@ -5,14 +5,14 @@ function BP3_arealiftbusesweep(vol, cl)
     if nargin>0
     	callStr=sprintf('areabusesweeplift(%.3f, %.2f)',vol,cl);
     else
-    	callStr = test_areabusesweeplift;
+    	callStr = 'test_areabusesweeplift';
     end
     disp(callStr);
     
     % [~,pathHome]=system('echo -n $HOME');
     % restartPath=[pathHome,'/SnakVolParam/restarts/restart_',...
     %     sprintf('areabuse_vol_%.3f',vol),'.mat'];
-    
+
     
     ExecuteOptimisation(callStr);
 
